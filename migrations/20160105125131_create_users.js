1973150693008afc7a314869e9a654b492b51d1a
davidtadams <adams.t.david@gmail.com>
2016-01-05 12:55:48 -0700

    add create users table knex migration

---
+++ migrations/20160105125131_create_users.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+    table.string('email').unique();
+    table.string('password');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('user');
+};
